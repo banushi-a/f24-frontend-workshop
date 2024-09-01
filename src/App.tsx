9cd4a104970158c0798cc8af9c2e778bedd8fd91
Alton Banushi <altonbanushi@icloud.com>
2024-09-01 16:38:45 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,6 @@ const App = () => {
       <header className="bg-yellow-100 w-screen py-2 px-4">
         <ul className="text-xl flex justify-between items-center">
           <li className="text-green-700 text-3xl font-bold">🐣 Chirper</li>
-          <li>{/* TODO: Link here */}</li>
         </ul>
       </header>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
